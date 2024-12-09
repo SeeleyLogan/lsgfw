e09668874df62e8211449ab313150d78c3a0d4a3
Logan Seeley <150400211+SeeleyLogan@users.noreply.github.com>
2024-12-08 21:40:11 -0500

    Added MSVC Support

---
+++ bin/CMakeFiles/lsgfw.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lsgfw.dir/link.d"
+  "CMakeFiles/lsgfw.dir/src/lsgfw.c.o"
+  "CMakeFiles/lsgfw.dir/src/lsgfw.c.o.d"
+  "liblsgfw.pdb"
+  "liblsgfw.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lsgfw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bin/tests/cube/CMakeFiles/cube.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cube.dir/link.d"
+  "CMakeFiles/cube.dir/src/main.c.o"
+  "CMakeFiles/cube.dir/src/main.c.o.d"
+  "cube"
+  "cube.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
